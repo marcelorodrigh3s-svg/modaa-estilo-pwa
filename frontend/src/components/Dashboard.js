5d1d740b966cd43e9710a04b3b83fbe706b1450f
marcelorodrigh3s-svg <marcelorodrigh3s@gmail.com>
2026-04-28 17:16:57 -0300

    Create Dashboard.js

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div>
+            <h2>📊 Dashboard</h2>
+            <p>Resumo da loja</p>
+        </div>
+    );
+}
+
+export default Dashboard;
